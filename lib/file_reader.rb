7931ac8f946c7f0a0c2c49ad59cda8f432042fd1
Zach Prince <zachary.s.prince@outlook.com>
2022-04-21 19:53:45 -0400

    Add read method test

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,11 +1,1 @@
-require 'rspec'
-require_relative '../lib/file_reader'
 
-RSpec.describe FileReader do
-  describe 'object' do
-    it 'exists' do
-      file_reader = FileReader.new
-      expect(file_reader).to be_an_instance_of FileReader
-    end
-  end
-end
--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -1,2 +1,21 @@
-class FileReader
+require 'rspec'
+require_relative '../lib/file_reader'
+
+RSpec.describe FileReader do
+  describe 'object' do
+    it 'exists' do
+      file_reader = FileReader.new
+      expect(file_reader).to be_an_instance_of FileReader
+    end
+  end
+
+  describe 'behaviors' do
+    before :each do
+      @file_reader = FileReader.new
+    end
+
+    it 'can read files' do
+      expect(@file_reader.read).to eq 'sphinx of black quartz judge my vow'
+    end
+  end
 end
